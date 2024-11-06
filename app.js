bc5e1fe9a9a8a2e9023ecdba36da0266a05f295f
Varinder Dhillon <varinderdhilloninfo@gmail.com>
2024-11-06 23:11:41 +0530

    Learned API, REST API's, How to design RESTful API and more. Created the first rest api

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 const port = 8000;
 
-app.get("/", (req, res)=>{
-    res.status(200).send("Hello from the server side!!!")
+// app.get("/", (req, res)=>{
+//     res.status(200).send("Hello from the server side!!!")
+// })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8'))
+
+app.get("/api/v1/tours", (req, res)=>{
+    res.status(200).json({
+        status: 'success',
+        result: tours.length,
+        data: {
+            tours
+        }
+    })
 })
 
 app.listen(port, ()=>{
