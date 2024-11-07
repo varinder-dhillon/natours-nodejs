ebd2484230a47d4f4fc4c3463b7af9b2bfd2c7fc
Varinder Dhillon <varinderdhilloninfo@gmail.com>
2024-11-07 21:53:17 +0530

    Created POST request add created a tour with that

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const port = 8000;
 //     res.status(200).send("Hello from the server side!!!")
 // })
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8'))
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'))
 
 app.get("/api/v1/tours", (req, res)=>{
     res.status(200).json({
@@ -17,6 +19,24 @@ app.get("/api/v1/tours", (req, res)=>{
             tours
         }
     })
+})
+
+app.post("/api/v1/tours", (req, res)=>{
+    // console.log(req.body)
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: "success",
+            data:{
+                tour: newTour
+            }
+        })
+    })
+
 })
 
 app.listen(port, ()=>{
